6d284afa049ed9b2e6e6cc5ea179694c45a21390
Remi Rousselet <darky12s@gmail.com>
2022-08-23 20:47:09 +0200

    Update third

--- third/lib/search_page.dart
+++ third/lib/search_page.dart
@@ -4,19 +4,24 @@ import 'package:pub_app/detail.dart';
 import 'package:pub_app/pub_ui/appbar.dart';
 import 'package:pub_app/pub_ui/package_item.dart';
 import 'package:pub_app/pub_ui/searchbar.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'search_page.g.dart';
 
 class Package {
   Package(this.name);
   final String name;
 }
 
-final packages = Provider<List<Package>>((ref) {
+@riverpod
+List<Package> fetchPackages(FetchPackagesRef ref) {
   return [
     Package('riverpod'),
     Package('freezed'),
     Package('flutter_hooks'),
+    // TODO show hot-reload
   ];
-});
+}
 
 class SearchPage extends ConsumerWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -31,7 +36,7 @@ class SearchPage extends ConsumerWidget {
           Expanded(
             child: ListView(
               children: [
-                for (final package in ref.watch(packages))
+                for (final package in ref.watch(FetchPackagesProvider))
                   PackageItem(
                     name: package.name,
                     onTap: () => Navigator.push(
